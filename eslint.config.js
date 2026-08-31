b7e1f81ea7cc69ac659bcc6822b329d3d6bbc572
panfiva <vadim.panfilov@gmail.com>
2026-08-31 10:09:32 -0500

    bump deps; use node-next

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+
+import tsParser from '@typescript-eslint/parser'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node,
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        projectService: true,
+      },
+    },
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 0,
+      '@typescript-eslint/no-unused-vars': [
+        1,
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+          argsIgnorePattern: '^_.*',
+          varsIgnorePattern: '^_.*',
+          destructuredArrayIgnorePattern: '^_.*',
+
+          caughtErrors: 'none',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/no-unused-expressions': 0,
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 0,
+      '@typescript-eslint/ban-ts-comment': 0,
+      'prefer-const': [1, { destructuring: 'all' }],
+      'no-unsafe-optional-chaining': 0,
+      'no-prototype-builtins': 0,
+      '@typescript-eslint/no-namespace': 0,
+      '@typescript-eslint/no-require-imports': 1,
+    },
+  },
+])
